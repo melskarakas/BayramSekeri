a8f3cd00b93ecf4ed80d4b180dde28383a3a09d7
YagizcanAkgun <akgunyagizcan84@gmail.com>
2026-03-15 11:24:12 +0300

    Lorem İpsum

--- BayramSekeri/App_Start/RouteConfig.cs
+++ BayramSekeri/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BayramSekeri
 {
@@ -17,6 +18,7 @@ namespace BayramSekeri
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                // What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             );
         }
     }
