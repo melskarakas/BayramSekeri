6f2d12e8a49b6f9dcc5eb4899dc5b0a8302183e8
Melis Karakas <melskarakas@gmail.com>
2026-03-28 18:59:35 +0300

    Fixed #13 -Value Controller eklendi

---
+++ BayramSekeri/Controllers/ValueController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BayramSekeri.Controllers
+{
+    public class ValueController : Controller
+    {
+        // GET: Value
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
