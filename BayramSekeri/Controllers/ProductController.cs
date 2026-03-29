b698e3a15440acc5a3ab9d9e69f9f9fce3dcef76
Meltem Yildiz <mltm_yildiz@outlook.com>
2026-03-29 11:16:42 +0300

    fixed #20

---
+++ BayramSekeri/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BayramSekeri.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllIndex()
+        {
+
+            return View();
+        }
+
+    }
+}
